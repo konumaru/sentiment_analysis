e942773431a8e05cc9853005083714031f8d5e5f
konumaru <konumaru1022@gmail.com>
2020-07-31 15:06:02 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# AutoML を使って感情分析をする
+
+## Step
+1. `app_store_crawler.gas` の getReview 関数を使ってよしなにレビューデータを取得
---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv('AppStoreReview.csv')
+
+print(df.shape)
+print(df.head())
